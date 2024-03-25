2580063d38e2aada00e9916347c5ed0d7db543ca
W0475240 <W0475240@nscc.ca>
2024-03-25 06:58:29 +0330

    Add Models

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models;
+
+public class Book
+{
+    [Key]
+    public required string ISBN { get; set; }
+
+    public required string Title { get; set; }
+
+    public required string Description { get; set; }
+
+    public int Price { get; set; }
+
+    public string Image { get; set; }
+
+    public int UserId { get; set; }
+
+    public User User { get; set; }
+}
---
+++ BookStore/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public required string Username { get; set; }
+    
+    public required string Password { get; set; }
+
+    public ICollection<Book>? Books { get; set; }
+}
